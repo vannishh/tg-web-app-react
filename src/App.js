d244580cc7bb57b7e4d5d6c63f253e21226e13fd
vannishh <istvanishoff@yandex.ru>
2023-03-21 19:44:21 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
         <Header/>
-        <button onClick={onToggleButton}>Закрыть</button>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
