df2d275394abe3f8997ead56a11fbcbe999d5fb9
vannishh <istvanishoff@yandex.ru>
2023-03-22 20:19:56 +0300

    productList

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import Button  from '../Button/Button'
-import './ProductItem.css'
+import Button from "../Button/Button";
+import './ProductItem.css';
 
-const ProductItem = ({product,className,onAdd}) => {
+const ProductItem = ({product, className, onAdd}) => {
 
     const onAddHandler = () => {
-        onAdd(product)
+        onAdd(product);
     }
 
     return (
-        <div className={'product' + className}>
-            <div className={"img"}></div>
-            <div className={"title"}>{product.title}</div>
-            <div className={"description"}>{product.description}</div>
-            <div className={"price"}>
+        <div className={'product ' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
