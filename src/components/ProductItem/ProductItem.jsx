060e5e8e969327af3d96dd8e23ae59f93372a4f4
vannishh <istvanishoff@yandex.ru>
2023-03-22 20:16:46 +0300

    productList

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -10,10 +10,10 @@ const ProductItem = ({product,className,onAdd}) => {
 
     return (
         <div className={'product' + className}>
-            <div className="img"></div>
-            <div className="title">{product.title}</div>
-            <div className="description">{product.description}</div>
-            <div className="price">
+            <div className={"img"}></div>
+            <div className={"title"}>{product.title}</div>
+            <div className={"description"}>{product.description}</div>
+            <div className={"price"}>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
